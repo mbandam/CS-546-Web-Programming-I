7bc01477baf62b046637aa09430b3ebfa43d3851
Mounika Bandam <bandam.mounika442@gmail.com>
2017-11-08 10:13:34 -0500

    Add files via upload

---
+++ Lab7/config/mongoConnection.js
@@ -0,0 +1,18 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const settings = {
+  mongoConfig: {
+      serverUrl: "mongodb://localhost:27017/",
+      database: "lab7-recipes"
+  }
+};
+
+let fullMongoUrl = settings.mongoConfig.serverUrl + settings.mongoConfig.database;
+let _connection = undefined;
+
+module.exports = async () => {
+    if (!_connection) {
+        _connection = await MongoClient.connect(fullMongoUrl);
+    }
+    return _connection;
+}
